73642970255bd123573bec5339406d485714bb71
Junsung Park <junsung.michael@gmail.com>
2015-07-31 03:08:03 +0900

    Create simple_adding.rb

---
+++ simple_adding.rb
@@ -0,0 +1,12 @@
+def SimpleAdding(num)
+  for i in (0..num-1) do
+    num = num + i
+  end
+  # code goes here
+  return num 
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+SimpleAdding(STDIN.gets)  
